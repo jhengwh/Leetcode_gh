199844ddb003a37314554db47a25d22dfe2a3379
jhengwh <chrishu.twn@gmail.com>
2024-03-09 00:01:55 +0800

    New post

---
+++ 005_104_Maximum Depth of Binary Tree/104.c
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+// Divide-and-conquer
+// Recursive
+
+/* Organized */
+int maxDepth(struct TreeNode* root)
+{
+    if (root == NULL)
+        return 0;
+    int D_left = maxDepth(root->left);
+    int D_right = maxDepth(root->right);
+    D = (D_left > D_right) ? D_left + 1 : D_right + 1;
+    return D;
+}
