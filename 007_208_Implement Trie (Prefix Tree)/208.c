e98b90114cd2adb99918beaf5867fe813bfbf263
jhengwh <chrishu.twn@gmail.com>
2024-03-10 18:13:43 +0800

    New post

--- 007_208_Implement Trie (Prefix Tree)/208.c
+++ 007_208_Implement Trie (Prefix Tree)/208.c
@@ -1,28 +1,87 @@
-/* Leetcode */
+#define ARRAY_SIZE(a) sizeof(a) / sizeof(a[0])
+// Alphabet size (# of symbols)
+#define ALPHABET_SIZE (26)
+// Converts key current character into index
+// use only 'a' through 'z' and lower case
+#define CHAR_TO_INDEX(c) ((int)c - (int)'a')
 
-// typedef struct _Trie {
+typedef struct _Trie {
+    struct Trie* children[ALPHABET_SIZE];
+    // isEndOfWord is true if the node represents
+    // end of a word
+    bool isEndOfWord;
+} Trie;
 
-// } Trie;
+Trie* trieCreate()
+{
+    Trie* pNode = NULL;
+    pNode = (Trie*)malloc(sizeof(Trie));
+    if (pNode) {
+        int i;
+        pNode->isEndOfWord = false;
+        for (i = 0; i < ALPHABET_SIZE; i++)
+            pNode->children[i] = NULL;
+    }
+    return pNode;
+}
 
-// Trie* trieCreate()
-// {
-// }
+void trieInsert(Trie* obj, char* word)
+{
+    int length = strlen(word);
+    int index;
+    Trie* pCrawl = obj;
 
-// void trieInsert(Trie* obj, char* word)
-// {
-// }
+    for (int level = 0; level < length; level++) {
+        index = CHAR_TO_INDEX(word[level]);
+        if (!pCrawl->children[index])
+            pCrawl->children[index] = trieCreate();
+        pCrawl = pCrawl->children[index];
+    }
+    // mark last node as leaf
+    pCrawl->isEndOfWord = true;
+}
 
-// bool trieSearch(Trie* obj, char* word)
-// {
-// }
+bool trieSearch(Trie* obj, char* word)
+{
+    int length = strlen(word);
+    int index;
+    Trie* pCrawl = obj;
 
-// bool trieStartsWith(Trie* obj, char* prefix)
-// {
-// }
+    for (int level = 0; level < length; level++) {
+        index = CHAR_TO_INDEX(word[level]);
+        if (!pCrawl->children[index])
+            return false;
+        pCrawl = pCrawl->children[index];
+    }
+    return (pCrawl->isEndOfWord);
+}
 
-// void trieFree(Trie* obj)
-// {
-// }
+bool trieStartsWith(Trie* obj, char* prefix)
+{
+    int length = strlen(prefix);
+    int index;
+    Trie* pCrawl = obj;
+
+    for (int level = 0; level < length; level++) {
+        index = CHAR_TO_INDEX(prefix[level]);
+        if (!pCrawl->children[index])
+            return false;
+        pCrawl = pCrawl->children[index];
+    }
+    return true;
+}
+
+// Free all the nodes recursively
+void trieFree(Trie* obj)
+{
+    if (obj == NULL)
+        return;
+    for (int i = 0; i < ALPHABET_SIZE; i++)
+        trieFree(obj->children[i]);
+
+    free(obj);
+    return;
+}
 
 /**
  * Your Trie struct will be instantiated and called as such:
@@ -35,118 +94,3 @@
 
  * trieFree(obj);
 */
-
-// ------------------------------------------------------------
-
-/* GeeksforGeeks */
-
-// C implementation of search and insert operations
-// on Trie
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#define ARRAY_SIZE(a) sizeof(a) / sizeof(a[0])
-
-// Alphabet size (# of symbols)
-#define ALPHABET_SIZE (26)
-
-// Converts key current character into index
-// use only 'a' through 'z' and lower case
-#define CHAR_TO_INDEX(c) ((int)c - (int)'a')
-
-// trie node
-struct TrieNode {
-    struct TrieNode* children[ALPHABET_SIZE];
-
-    // isEndOfWord is true if the node represents
-    // end of a word
-    bool isEndOfWord;
-};
-
-// Returns new trie node (initialized to NULLs)
-struct TrieNode* getNode(void)
-{
-    struct TrieNode* pNode = NULL;
-
-    pNode = (struct TrieNode*)malloc(sizeof(struct TrieNode));
-
-    if (pNode) {
-        int i;
-
-        pNode->isEndOfWord = false;
-
-        for (i = 0; i < ALPHABET_SIZE; i++)
-            pNode->children[i] = NULL;
-    }
-
-    return pNode;
-}
-
-// If not present, inserts key into trie
-// If the key is prefix of trie node, just marks leaf node
-void insert(struct TrieNode* root, const char* key)
-{
-    int level;
-    int length = strlen(key);
-    int index;
-
-    struct TrieNode* pCrawl = root;
-
-    for (level = 0; level < length; level++) {
-        index = CHAR_TO_INDEX(key[level]);
-        if (!pCrawl->children[index])
-            pCrawl->children[index] = getNode();
-
-        pCrawl = pCrawl->children[index];
-    }
-
-    // mark last node as leaf
-    pCrawl->isEndOfWord = true;
-}
-
-// Returns true if key presents in trie, else false
-bool search(struct TrieNode* root, const char* key)
-{
-    int level;
-    int length = strlen(key);
-    int index;
-    struct TrieNode* pCrawl = root;
-
-    for (level = 0; level < length; level++) {
-        index = CHAR_TO_INDEX(key[level]);
-
-        if (!pCrawl->children[index])
-            return false;
-
-        pCrawl = pCrawl->children[index];
-    }
-
-    return (pCrawl->isEndOfWord);
-}
-
-// Driver
-int main()
-{
-    // Input keys (use only 'a' through 'z' and lower case)
-    char keys[][8] = { "the", "a", "there", "answer", "any",
-        "by", "bye", "their" };
-
-    char output[][32] = { "Not present in trie", "Present in trie" };
-
-    struct TrieNode* root = getNode();
-
-    // Construct trie
-    int i;
-    for (i = 0; i < ARRAY_SIZE(keys); i++)
-        insert(root, keys[i]);
-
-    // Search for different keys
-    printf("%s --- %s\n", "the", output[search(root, "the")]);
-    printf("%s --- %s\n", "ther", output[search(root, "ther")]);
-    printf("%s --- %s\n", "their", output[search(root, "their")]);
-    printf("%s --- %s\n", "thaw", output[search(root, "thaw")]);
-
-    return 0;
-}
